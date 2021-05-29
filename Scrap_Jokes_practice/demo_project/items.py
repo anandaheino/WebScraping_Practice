865fca0f1567191d03923da5b9af858f7491141a
ananda <anandaheino2@gmail.com>
2021-05-29 17:06:48 -0300

    Removendo os espaços em branco antes dos textos obtidos

--- Scrap_Jokes_practice/demo_project/items.py
+++ Scrap_Jokes_practice/demo_project/items.py
@@ -7,12 +7,19 @@ import scrapy
 from scrapy.loader.processors import MapCompose, TakeFirst
 from w3lib.html import remove_tags
 
+
+def remove_whitespace(value):
+    return value.strip()
+
 class JokeItem(scrapy.Item):
     # new field that will hold the joke text
     joke_text = scrapy.Field(
-        input_processor = MapCompose(remove_tags),
+        input_processor = MapCompose(remove_tags, remove_whitespace),
         output_processor = TakeFirst()
     )
     # MapCompose: class that takes methods as arguments and applies to the data being scraped
     # Input and Output processors: they clean the data
     # TakeFirst() -> same as extract_first()
+
+    # to remove white spaces before the jokes: add a function to the MapCompose
+    # creating a remove_whitespace function and using inside MapCompose
