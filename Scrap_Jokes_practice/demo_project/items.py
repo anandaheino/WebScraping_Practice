26be25eaa8e32516b24badee72cff12247a47b94
ananda <anandaheino2@gmail.com>
2021-05-29 17:00:39 -0300

    Implementando proessadores input e output para limpar os dados obtidos

--- Scrap_Jokes_practice/demo_project/items.py
+++ Scrap_Jokes_practice/demo_project/items.py
@@ -4,9 +4,15 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+from w3lib.html import remove_tags
 
-
-class DemoProjectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class JokeItem(scrapy.Item):
+    # new field that will hold the joke text
+    joke_text = scrapy.Field(
+        input_processor = MapCompose(remove_tags),
+        output_processor = TakeFirst()
+    )
+    # MapCompose: class that takes methods as arguments and applies to the data being scraped
+    # Input and Output processors: they clean the data
+    # TakeFirst() -> same as extract_first()
--- Scrap_Jokes_practice/demo_project/spiders/jokes.py
+++ Scrap_Jokes_practice/demo_project/spiders/jokes.py
@@ -1,5 +1,6 @@
 import scrapy
-
+from demo_project.items import JokeItem
+from scrapy.loader import ItemLoader
 
 class JokesSpider(scrapy.Spider):
     name = 'jokes'   # we can call/launch this spider from the terminal using this name
@@ -9,9 +10,11 @@ class JokesSpider(scrapy.Spider):
 
     def parse(self, response):
         for joke in response.xpath("//div[@class='jokes']"):
-            yield {
-                'joke_text': joke.xpath(".//div[@class='joke-text']/p/text()").extract_first()
-            }
+            # Instatiating the ItemLoader class:
+            loader = ItemLoader(item = JokeItem(), selector=joke)
+            loader.add_xpath('joke_text', ".//div[@class='joke-text']/p")    # add_xpath('field_name', 'xpath_expression')
+            yield loader.load_item()    # calling this method from ItemLoader class
+
         next_page = response.xpath("//li[@class='next']/a/@href").extract_first()
         if next_page is not None:
             next_page_link = response.urljoin(next_page)
