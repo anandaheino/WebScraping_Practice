8f29a1bf665900c3c2e04b7f9c7e05785b68b29a
ananda <anandaheino2@gmail.com>
2021-05-31 17:44:34 -0300

    Inserindo Processors classes: MapCompose, Join. Adição de Docstring: Contract

--- Learning_scrapy_Basic/Learning_scrapy_Basic/spiders/basic.py
+++ Learning_scrapy_Basic/Learning_scrapy_Basic/spiders/basic.py
@@ -1,6 +1,11 @@
 import scrapy
 from Learning_scrapy_Basic.items import PropertiesItem
 from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, Join
+import urlparse
+import datetime
+import socket
+
 
 class BasicSpider(scrapy.Spider):
 
@@ -10,13 +15,23 @@ class BasicSpider(scrapy.Spider):
 
     def parse(self, response):
 
+        """This function parses a property page.
+        @url http://web:9312/properties/property_000000.html
+        @returns items 1
+        @scrapes title price description address image_urls
+        @scrapes url project spider server date"""
+
         loader = ItemLoader(item = PropertiesItem(), response= response)  # instantiating
 
-        loader.add_xpath('//*[@itemprop="name"][1]/text()').extract()
-        loader.add_xpath('//*[@itemprop="price"][1]/text()').re('[.0-9]+')
-        loader.add_xpath('//*[@itemprop="description"][1]/text()').extract()
-        loader.add_xpath('//*[@itemtype="http://schema.org/"Place"][1]/text()').extract()
-        loader.add_xpath('//*[@itemprop="image"][1]/@src').extract()
+        loader.add_xpath('title', '//*[@itemprop="name"][1]/text()', MapCompose(unicode.strip, unicode.title))
+        loader.add_xpath('price', '//*[@itemprop="price"][1]/text()', MapCompose(lambda i: i.replace(',', ''), float), re='[,.0-9]+')
+        loader.add_xpath('description', '//*[@itemprop="description"][1]/text()', MapCompose(unicode.strip), Join())
+        loader.add_xpath('address', '//*[@itemtype="http://schema.org/"Place"][1]/text()', MapCompose(unicode.strip))
+        loader.add_xpath('image_urls', '//*[@itemprop="image"][1]/@src', MapCompose(lambda i: urlparse.urljoin(response.url, i)))
+
+        loader.add_value('url', response.url)
+        loader.add_value('project', self.settings.get('BOT_NAME'))
+        loader.add_value('server', socket.gethostname())
+        loader.add_value('date', datetime.datetime.now())
 
         return loader.load_item()
-    
