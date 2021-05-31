457a8000d23be69447a933865c969170273cce4e
ananda <anandaheino2@gmail.com>
2021-05-31 15:23:32 -0300

    Criando projeto scrapy e definindo items.py

---
+++ Learning_scrapy_Basic/Learning_scrapy_Basic/items.py
@@ -0,0 +1,20 @@
+from scrapy.item import Item, Field
+
+
+class PropertiesItem(Item):
+    # defyning the fields for the item here:
+    title = Field()
+    price = Field()
+    description = Field()
+
+    # Other fields that might be important:
+    images = Field()
+    location = Field()
+
+    # other very usefull fields:
+    url = Field()
+    project = Field()
+    spider = Field()
+    server = Field()
+    date = Field()
+    
